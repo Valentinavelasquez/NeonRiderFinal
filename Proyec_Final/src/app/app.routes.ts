f09737716c9a6a42dd5dede9d57729ed9161a429
Valentinavelasquez <133972083+Valentinavelasquez@users.noreply.github.com>
2025-08-25 21:55:01 -0500

    creacion de footer

--- Proyec_Final/src/app/app.routes.ts
+++ Proyec_Final/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { Footer } from './home/footer/footer';
+import { Nabvar } from './home/nabvar/nabvar';
 
 export const routes: Routes = [];
+
--- Proyec_Final/src/app/app.ts
+++ Proyec_Final/src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Footer } from "./home/footer/footer";
+import { Nabvar } from "./home/nabvar/nabvar";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Footer, Nabvar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
