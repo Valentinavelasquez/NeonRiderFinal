c1279d2e03f42dfefd098a57c0dcff378fb5e45e
Valentinavelasquez <133972083+Valentinavelasquez@users.noreply.github.com>
2025-08-26 14:45:18 -0500

    creacion de carrusel, dashboard

--- Proyec_Final/src/app/app.ts
+++ Proyec_Final/src/app/app.ts
@@ -2,10 +2,12 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Footer } from "./home/footer/footer";
 import { Nabvar } from "./home/nabvar/nabvar";
+import { Dashboard } from "./home/dashboard/dashboard";
+import { Carrusel } from "./home/carrusel/carrusel";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Footer, Nabvar],
+  imports: [RouterOutlet, Footer, Nabvar, Dashboard, Carrusel],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
