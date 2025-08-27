e983257a8ac8186573df02df91b32ff173f4cee3
Valentinavelasquez <133972083+Valentinavelasquez@users.noreply.github.com>
2025-08-27 15:04:20 -0500

    creacion de loguin y registro, se realizaron los ruteos

--- Proyec_Final/src/app/app.routes.ts
+++ Proyec_Final/src/app/app.routes.ts
@@ -1,6 +1,15 @@
 import { Routes } from '@angular/router';
 import { Footer } from './home/footer/footer';
 import { Nabvar } from './home/nabvar/nabvar';
+import { Loguin } from './public/loguin/loguin';
+import { Dashboard } from './home/dashboard/dashboard';
+import { Registro } from './public/registro/registro';
+import { Carrusel } from './home/carrusel/carrusel';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:"loguin", component:Loguin},
+  {path:"carrusel", component:Carrusel},
+  {path:"dashboard", component:Dashboard},
+  {path:"registro", component:Registro}
+];
 
