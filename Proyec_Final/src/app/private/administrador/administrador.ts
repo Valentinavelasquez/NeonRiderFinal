f540395336d023c878284d7fc452c9d4c127f371
Valentinavelasquez <133972083+Valentinavelasquez@users.noreply.github.com>
2025-08-28 19:45:07 -0500

    creacion perfil administrador y formulario

---
+++ Proyec_Final/src/app/private/administrador/administrador.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-administrador',
+  imports: [],
+  templateUrl: './administrador.html',
+  styleUrl: './administrador.css'
+})
+export class Administrador {
+
+}
