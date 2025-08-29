eb4264b6b75d9f601bdca37915a30642f49dbfcd
Valentinavelasquez <133972083+Valentinavelasquez@users.noreply.github.com>
2025-08-28 19:45:07 -0500

    creacion perfil administrador y formulario

--- Proyec_Final/src/app/app.routes.ts
+++ Proyec_Final/src/app/app.routes.ts
@@ -3,11 +3,15 @@ import { Loguin } from './public/loguin/loguin';
 import { Dashboard } from './home/dashboard/dashboard';
 import { Registro } from './public/registro/registro';
 import { Carrusel } from './home/carrusel/carrusel';
+import { Component } from '@angular/core';
+import { Administrador } from './private/administrador/administrador';
+import { RouterLink } from '@angular/router';
 
 export const routes: Routes = [
   {path:"loguin", component:Loguin},
   {path:"dashboard", component:Dashboard},
   {path:"carrusel", component:Carrusel},
-  {path:"registro", component:Registro}
+  {path:"registro", component:Registro},
+  {path:"administrador", component:Administrador},
 ];
 
--- Proyec_Final/src/app/home/nabvar/nabvar.ts
+++ Proyec_Final/src/app/home/nabvar/nabvar.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-nabvar',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './nabvar.html',
   styleUrl: './nabvar.css'
 })
 export class Nabvar {
-
 }
