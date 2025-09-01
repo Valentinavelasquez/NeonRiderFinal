c9505a25507835b49641a1800853b53047a24709
Daniel Rativa <danielrativa26@gmail.com>
2025-09-01 16:30:52 -0500

    login y registro completo

--- Proyec_Final/src/app/app.routes.ts
+++ Proyec_Final/src/app/app.routes.ts
@@ -1,13 +1,17 @@
 import { Routes } from '@angular/router';
+import { Footer } from './home/footer/footer';
+import { Nabvar } from './home/nabvar/nabvar';
 import { Loguin } from './public/loguin/loguin';
 import { Dashboard } from './home/dashboard/dashboard';
 import { Registro } from './public/registro/registro';
 import { Carrusel } from './home/carrusel/carrusel';
 
 export const routes: Routes = [
-  {path:"loguin", component:Loguin},
-  {path:"dashboard", component:Dashboard},
+  {path:"", redirectTo:"dashboard", pathMatch: "full"},
+  {path:"loguin", component:Loguin, pathMatch: "full"},
   {path:"carrusel", component:Carrusel},
-  {path:"registro", component:Registro}
+  {path:"dashboard", component:Dashboard},
+  {path:"registro", component:Registro},
+  { path: "**", redirectTo: "error-404", pathMatch: "full"}
 ];
 
