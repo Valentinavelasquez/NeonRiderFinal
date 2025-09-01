e1d56419035cc26917d0dfe4482f7d9f1fb3d549
Valentinavelasquez <133972083+Valentinavelasquez@users.noreply.github.com>
2025-09-01 17:15:30 -0500

    redireccionamiento de rutas  creacion de cards de textil, y accesorios

--- Proyec_Final/src/app/home/dashboard/dashboard.ts
+++ Proyec_Final/src/app/home/dashboard/dashboard.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Carrusel } from "../carrusel/carrusel";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [Carrusel],
+  imports: [Carrusel,RouterLink],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css'
 })
