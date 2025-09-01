824047614af8c7e72d5e120bb2bf62502c49bd3b
carlosMrtnzMrtnz <carlosmartinezmartinez2015@gmail.com>
2025-09-01 18:31:22 -0500

    creacion de cascos

--- Proyec_Final/src/app/home/cascos/cascos.ts
+++ Proyec_Final/src/app/home/cascos/cascos.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Product } from '../../services/product/product';
 
 @Component({
   selector: 'app-cascos',
@@ -7,5 +8,26 @@ import { Component } from '@angular/core';
   styleUrl: './cascos.css'
 })
 export class Cascos {
+
+  productService = inject(Product)
+  items!:any
+  ngOnInit() {
+    this.renderCascos()
+  }
+
+  renderCascos() {
+    this.productService.getCascos().subscribe({
+      next:(dataApi:any)=> {
+        this.items= dataApi
+      },
+      error:(error:any)=> {
+        console.log(error);
+
+      }
+    })
+  }
+
+
+
 
 }
