eb61d4877490c6c307f97349fb2065cdef6871a2
carlosMrtnzMrtnz <carlosmartinezmartinez2015@gmail.com>
2025-09-01 18:31:22 -0500

    creacion de cascos

--- Proyec_Final/src/app/private/administrador/administrador.ts
+++ Proyec_Final/src/app/private/administrador/administrador.ts
@@ -1,12 +1,67 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
+import { Product } from '../../services/product/product';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-administrador',
-  imports: [],
+  imports: [
+    ReactiveFormsModule
+  ],
   templateUrl: './administrador.html',
   styleUrl: './administrador.css'
 })
 export class Administrador {
+
+  productService = inject(Product)
+
+
+  formProduct !: FormGroup
+
+  constructor(private fb: FormBuilder) {
+    this.formProduct = fb.group({
+      Imagen:"",
+      Descripcion:"",
+      referencia:"",
+      Marca: "",
+      Tipo:"",
+      Color: "Azul",
+      Precio:""
+    })
+  }
+
+
+  ngOnInit() {
+
+  }
+
+  createProduct() {
+    console.log(this.formProduct.value);
+    this.productService.createProduct(this.formProduct.value).subscribe({
+      next:(dataApi: any)=> {
+              Swal.fire({
+                title:"¡Creado!",
+                icon:"success",
+                text:"Producto Creado!",
+                draggable:true
+              })
+
+              this.formProduct.reset()
+      },
+      error:(error:any)=> {
+              Swal.fire({
+                title:"¡Error!",
+                icon:"warning",
+                text:"El Producto no se pudo crear!",
+                draggable:true
+              })
+      }
+    })
+
+
+  }
+
+
 
 }
