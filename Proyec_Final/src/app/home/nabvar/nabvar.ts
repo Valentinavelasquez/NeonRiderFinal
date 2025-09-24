4ad5251385b30394537807a94fd27fadeefa5c26
Daniel Rativa <danielrativa26@gmail.com>
2025-09-23 20:01:57 -0500

    update

--- Proyec_Final/src/app/home/nabvar/nabvar.ts
+++ Proyec_Final/src/app/home/nabvar/nabvar.ts
@@ -8,4 +8,18 @@ import { RouterLink } from '@angular/router';
   styleUrl: './nabvar.css'
 })
 export class Nabvar {
+loguiado :boolean = !!sessionStorage.getItem('token')
+  ngOnInit(){
+    this.loguiado=!!sessionStorage.getItem('token')
+  }
+
+  ngDoCheck(){
+    this.loguiado=!!sessionStorage.getItem('token')
+
+  }
+
+  logout(){
+    sessionStorage.removeItem('token')
+    sessionStorage.removeItem('user')
+  }
 }
