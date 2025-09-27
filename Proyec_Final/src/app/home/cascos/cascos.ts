e1c013e6e284f36d2a786b8c0a9663db92b8d211
Valentinavelasquez <133972083+Valentinavelasquez@users.noreply.github.com>
2025-09-27 00:40:02 -0500

    creacion de parametro para visualizacion modificacion en home

--- Proyec_Final/src/app/home/cascos/cascos.ts
+++ Proyec_Final/src/app/home/cascos/cascos.ts
@@ -1,9 +1,10 @@
 import { Component, inject } from '@angular/core';
 import { Product } from '../../services/product/product';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-cascos',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './cascos.html',
   styleUrl: './cascos.css'
 })
@@ -11,23 +12,27 @@ export class Cascos {
 
   productService = inject(Product)
   items!:any
+constructor(private router: ActivatedRoute) {}
+
   ngOnInit() {
-    this.renderCascos()
+
+    this.renderCascos(this.router.snapshot.params['categoria'])
   }
 
-  renderCascos() {
-    this.productService.getCascos().subscribe({
-      next:(dataApi:any)=> {
-        this.items= dataApi
-      },
-      error:(error:any)=> {
-        console.log(error);
-
+  renderCascos(categoria: string) {
+  this.productService.getCascos().subscribe({
+    next: (dataApi: any) => {
+      if (categoria === 'todos') {
+        this.items = dataApi; // muestra todo
+      } else {
+        this.items = dataApi.filter((casco: any) => casco.categoria === categoria);
       }
-    })
-  }
-
-
+    },
+    error: (error: any) => {
+      console.error(error);
+    }
+  });
+}
 
 
 }
